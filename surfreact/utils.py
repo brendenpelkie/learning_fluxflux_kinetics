d1414bfacac3e3d28cfadf3150525dbec33ffa77
Brenden Pelkie <bgpelkie@gmail.com>
2022-09-26 13:04:25 -0700

    remove one more reaxnet ref

--- surfreact/utils.py
+++ surfreact/utils.py
@@ -22,8 +22,7 @@ def atoms_to_molmllist(atoms: Atoms, bonds: bool = False):
     """Constructs a molml list of lists from an atoms object.
 
     By default, will consider Atomic numbers and atom distances, but can be
-    directed to also compute bonds. See the reaxnet.io.rdkit for bond
-    determination.
+    directed to also compute bonds. 
 
     Parameters
     ----------
